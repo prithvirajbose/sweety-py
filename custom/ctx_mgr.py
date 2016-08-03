ab846a4d00247c6a85d829e708d5189f9c457cf4
prithvirajbose <prithvirajbose@hotmail.com>
2016-08-03 15:27:52 +0530

    added generator based context mgr in ctx_mgr.py

--- custom/ctx_mgr.py
+++ custom/ctx_mgr.py
@@ -1,5 +1,24 @@
+"""
+Demostrates how to use the context manager as a
+decorator and as a class in the context of a client socket.
+"""
 
 import socket
+from contextlib import contextmanager
+
+@contextmanager
+def socket_client_ctx(socketclient):
+      """
+      Demostrates how to use the context manager decorator
+      in the context of a client socket.
+      """
+      try:
+            socketclient.connect()
+            yield socketclient
+      except:
+            raise
+      finally:
+            socketclient.close()
 
 class SocketClient(object):
       """
@@ -18,24 +37,41 @@ class SocketClient(object):
             self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self._hostname = hostname
             self._port = port
-
-      def __enter__(self):
-            print '> inside __enter__, connecting to {0}/{1}'\
-                  .format(self._hostname, self._port)
             
+      def connect(self):
+            print '> inside {0}, connecting to {1}/{2}'.format(self.__class__, self._hostname, self._port)
             self._socket.connect((self._hostname, self._port))
-            return self
 
-      def __exit__(self, exception, value, traceback):
-            print '> inside __exit__, closing socket...'
+      def close(self):
             self._socket.close()
-            return False
-
+            
       def send(self, data):
-            print '> sending data: \'{0}\' to server...'.format(data)
+            print '> inside {0}, sending data: \'{1}\' to server...'.format(self.__class__, data)
             self._socket.send(data)
 
-if __name__ == '__main__':
-      with SocketClient() as s:
+class SocketClientContext(object):
+      """
+      Demostrates how to use the context manager in a context of the
+      client socket.
+      """
+      def __init__(self, socket_client):
+            self._socket_client = socket_client
+
+      def __enter__(self):
+            print '> inside {0}.__enter__...'.format(self.__class__)
+            self._socket_client.connect()
+            return self._socket_client
+
+      def __exit__(self, exception, value, traceback):
+            print '> inside {0}.__exit__...'.format(self.__class__)
+            self._socket_client.close()
+            return False
+
+if __name__ == '__main__':\
+      '''
+      This has the same effect:
+            with SocketClientContext(SocketClient())as s:
+      '''
+      with socket_client_ctx(SocketClient())as s:
             s.send('test data')
       
